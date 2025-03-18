f453429b8e03acfd77f1853c1b3df9022eaf1707
avika30 <singh.avika3010@gmail.com>
2025-03-18 18:28:09 +0530

    Initial Commit

---
+++ CppProgramming/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppProgramming
+//
+//  Created by Avika Singh on 3/18/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
